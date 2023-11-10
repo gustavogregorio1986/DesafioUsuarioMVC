efa38f5676e2e50cc9396c126cba2e150e52bcc0
guga56 <gustavogregorio086@gmail.com>
2023-11-10 15:53:44 -0300

    desenvolvendo po menu da ap-licação que vai conter o cadastro e consulat

---
+++ DesafioUsuario/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioUsuario.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}

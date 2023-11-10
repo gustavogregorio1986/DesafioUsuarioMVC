6852440f5f22b183da555d9279daca82dc221fb8
guga56 <gustavogregorio086@gmail.com>
2023-11-10 16:07:20 -0300

    criando o models para o frontend

---
+++ DesafioUsuario/Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DesafioUsuario.Models
+{
+    public class ClienteModel
+    {
+        public int IdUsuario { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public byte Upload { get; set; }
+    }
+}

fdab0a9b2dbe3d533a2081d69ff4e8a72c56227d
guga56 <gustavogregorio086@gmail.com>
2023-11-10 16:33:05 -0300

    cadstrao com os campos criados

--- DesafioUsuario/Models/ClienteModel.cs
+++ DesafioUsuario/Models/ClienteModel.cs
@@ -1,13 +1,18 @@
-﻿namespace DesafioUsuario.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioUsuario.Models
 {
     public class ClienteModel
     {
         public int IdUsuario { get; set; }
 
+        [Required(ErrorMessage = "Informe o Nome")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Informe o E-mail")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Informe a Senha")]
         public string Senha { get; set; }
 
         public byte Upload { get; set; }
